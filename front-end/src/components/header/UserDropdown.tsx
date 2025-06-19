f7aba23df7d2f8d733ab5e23add87d68252c20fc
Yubie <ayoub04bd@gmail.com>
2025-06-19 18:10:53 +0100

    fix: update user image source in UserDropdown component

--- front-end/src/components/header/UserDropdown.tsx
+++ front-end/src/components/header/UserDropdown.tsx
@@ -38,7 +38,7 @@ export default function UserDropdown() {
           <Image
             width={44}
             height={44}
-            src="/images/user/owner.jpg"
+            src="/images/user/user-10.jpg"
             alt="User"
           />
         </span>
