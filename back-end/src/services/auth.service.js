b23c4baa5a98df788a1a387d0ad44d4b1c63a229
YubieTheSAVAGE <ayoub04bd@gmail.com>
2025-06-25 05:02:32 +0100

    refactor: uncomment createUser function in auth.service.js and update user creation logic

--- back-end/src/services/auth.service.js
+++ back-end/src/services/auth.service.js
@@ -15,25 +15,25 @@ async function loginUser(email, password) {
   return user;
 } 
 
-// async function createUser() {
-//   try {
-//     const passwordHash = await bcrypt.hash("password123", 10);
-//     const user = await prisma.user.create({
-//       data: {
-//         name: "Test User",
-//         email: "issam@immo360.com",
-//         phoneNumber: "1234567890",
-//         role: "ADMIN",
-//         passwordHash
-//       }
-//     });
-//     console.log("User created successfully:", user);
-//   } catch (error) {
-//     console.error("Error creating user:", error);
-//   } finally {
-//     await prisma.$disconnect();
-//   }
-// }
+async function createUser() {
+  try {
+    const passwordHash = await bcrypt.hash("password123", 10);
+    const user = await prisma.user.create({
+      data: {
+        name: "Test User",
+        email: "issam@immo360.com",
+        phoneNumber: "1234567890",
+        role: "ADMIN",
+        passwordHash
+      }
+    });
+    console.log("User created successfully:", user);
+  } catch (error) {
+    console.error("Error creating user:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
 
 // createUser(); 
 
