414e0003841fae5d57be3a323750d9343f63e1a7
YubieTheSAVAGE <ayoub04bd@gmail.com>
2025-07-01 05:11:10 +0100

    refactor: standardize UI elements across data tables, update client data structure to include first and last names, and replace 'apartments' with 'properties' for consistency

--- front-end/src/components/tables/DataTables/Agents/AgentsDataTable.tsx
+++ front-end/src/components/tables/DataTables/Agents/AgentsDataTable.tsx
@@ -149,7 +149,7 @@ export default function AgentsDataTable({ agents, onClientEdit }: { agents: Agen
       <div className="flex flex-col gap-2 px-4 py-4 border border-b-0 border-gray-100 dark:border-white/[0.05] rounded-t-xl sm:flex-row sm:items-center sm:justify-between">
         <div className="flex items-center gap-3">
           <span className="text-gray-500 dark:text-gray-400"> Afficher </span>
-          <div className="relative ./src/components/tables/DataTables/Agents/AgentsDataTable.tsxz-20 bg-transparent">
+          <div className="relative z-0 bg-transparent">
             <select
               className="w-full py-2 pl-3 pr-8 text-sm text-gray-800 bg-transparent border border-gray-300 rounded-lg appearance-none dark:bg-dark-900 h-9 bg-none shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
               value={itemsPerPage}
